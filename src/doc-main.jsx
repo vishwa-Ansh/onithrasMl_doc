dc1d99e2bb795784c089a0a7c42931dd26a17a42
Ansh vishwakarma <vishwaansh244@gmail.com>
2026-08-25 19:14:03 +0530

    imporving layout

--- src/doc-main.jsx
+++ src/doc-main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { SiteHeader } from "../../src/components/SiteHeader";
+import { SiteHeader } from "./components/SiteHeader";
 
 const root = document.getElementById("header-root");
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+
+        docs: resolve(
+          __dirname,
+          "docs/index.html"
+        ),
+
+        numericalComputations: resolve(
+          __dirname,
+          "docs/modules/numerical_comutations/index.html"
+        ),
+
+        linearAlgebra: resolve(
+          __dirname,
+          "docs/modules/numerical_comutations/linear_algreba/index.html"
+        ),
+
+        linalg: resolve(
+          __dirname,
+          "docs/modules/numerical_comutations/linear_algreba/linalg/index.html"
+        ),
+
+        solve: resolve(
+          __dirname,
+          "docs/modules/numerical_comutations/linear_algreba/linalg/methods_class/solve.html"
+        ),
+      },
+    },
+  },
 });
