e02d060bae24b901424ecfd075b4029e2b04b01f
Ansh vishwakarma <vishwaansh244@gmail.com>
2026-08-25 20:31:10 +0530

    imporving layout

--- src/doc-main.jsx
+++ src/doc-main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { SiteHeader } from "./components/SiteHeader";
+import "./styles.css";
 
 const root = document.getElementById("header-root");
 
